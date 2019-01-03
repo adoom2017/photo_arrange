b7ec617f2bf42426b0c7829801fdf4aee789bab7
shendongchun <shendongchun08@gmail.com>
2019-01-03 16:58:36 +0800

    Update collect_picture.py

--- collect_picture.py
+++ collect_picture.py
@@ -1,38 +1,59 @@
+# -*- coding: utf-8 -*-
+
 import filecmp
 import getopt
 import logging
 import os
 import shutil
 import sys
-import time
+import traceback
 
-import exifread
-import ffmpeg
+import subprocess
+import json
 
 VIDEO_FOLD = "video"
-UNCLASSIFY_FOLD = "unclassify"
-CLASSIFY_FOLD = "photo"
+PHOTO_FOLD = "photo"
 MOVE_FILE = False
 
-class ReadFailException(Exception):
-    pass
+PhotoExtNames = ('.jpg','.png','.jpeg')
+VedioExtNames = ('.mp4','.mov','.avi')
+
+TimeFlag = u'CreateDate'
+
+def popen(cmd):
+    try:
+        popen = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        popen.wait()
+        lines = popen.stdout.readlines()
+        output = ""
+        for line in lines:
+            output += line.decode('utf-8').strip()
+        return output
+    except BaseException as e:
+        logging.exception(e)
+        return None
+
+def get_metadata(filename):
+    cmd = "exiftool -j " + filename
+    result = popen(cmd)
+    if result == None:
+        return None
+    else:
+        return json.loads(result)[0]
 
 def getOrignalDate(filename):
-    try:
-        fd = open(filename, 'rb')
-    except:
-        logging.error("Cannot open: %s." % filename)
-        raise ReadFailException
-    data = exifread.process_file(fd)
-    if data:
-        try:
-            t = data['EXIF DateTimeOriginal']
-            return str(t)
-        except:
-            logging.error("No exif: %s." % filename)
-            pass
+    meta = get_metadata(filename)
+    if meta == None:
+        logging.error("Can not found create date in meta for %s." % filename)
+        return None
 
-    return Null
+    if TimeFlag in meta:
+        cameraDate = meta[TimeFlag]
+    else:
+        logging.error("Can not found create date in meta for %s." % filename)
+        return None
+
+    return cameraDate
 
 def mainLoop(path):
     logging.info("Process %s." % path)
@@ -40,60 +61,50 @@ def mainLoop(path):
         for filename in files:
             absolute_file = os.path.join(root, filename)
             name, suffix = os.path.splitext(absolute_file)
-            if suffix.lower() in ('.jpg', '.png'):
-                classifyPictures(absolute_file)
-            elif suffix.lower() in ('.mov', '.mp4'):
-                classifyVideo(absolute_file)
+            if suffix.lower() in PhotoExtNames:
+                classifyAll(absolute_file, PHOTO_FOLD)
+            elif suffix.lower() in VedioExtNames:
+                classifyAll(absolute_file, VIDEO_FOLD)
             else:
-                logging.info("%s is not collected." % absolute_file)
+                logging.info("File: %s is not collected." % absolute_file)
 
-def copyFile(filename, dstFileName):
+def copyFile(srcFileName, dstFileName):
     i = 1
     while os.path.exists(dstFileName):
-        if filecmp.cmp(filename, dstFileName):
-            logging.info("%s already exist." % filename)
+        if filecmp.cmp(srcFileName, dstFileName):
+            logging.info("%s already exist." % srcFileName)
             return
         else:
             name, suffix = os.path.splitext(dstFileName)
             dstFileName = name + "_" + str(i) + suffix
             i = i + 1
 
-    logging.info("Collect File %s to %s." % (filename, dstFileName))
+    logging.info("Collect File %s to %s." % (srcFileName, dstFileName))
     if not os.path.exists(os.path.dirname(dstFileName)):
         os.makedirs(os.path.dirname(dstFileName))
     if MOVE_FILE:
-        shutil.move(filename, dstFileName)
+        shutil.move(srcFileName, dstFileName)
     else:
-        shutil.copy2(filename, dstFileName)
+        shutil.copy2(srcFileName, dstFileName)
 
-def classifyPictures(filename):
-        time=""
-        try:
-            time = getOrignalDate(filename)
-        except Exception:
-            logging.info("Can not get time for %s." % filename)
-            dstFileName = UNCLASSIFY_FOLD + "\\" + os.path.basename(filename)
-            copyFile(filename, dstFileName)
-            return
+def classifyAll(srcFileName, dstRootFold):
+    time = getOrignalDate(srcFileName)
+    if time != None:
+        name, suffix = os.path.splitext(srcFileName)
+        dstFileName = dstRootFold +'\\'+ time.replace(":", "-")[:7] + '\\'+ time.replace(":", "").replace(" ", "_") + suffix
+    else:
+        dstFileName = dstRootFold + "\\unclassify" + "\\" + os.path.basename(srcFileName)
 
-        name, suffix = os.path.splitext(filename)
-        dstFileName = CLASSIFY_FOLD +'\\'+ time.replace(":", "-")[:7] + '\\'+ time.replace(":", "").replace(" ", "_") + suffix
-
-        copyFile(filename, dstFileName)
-
-def classifyVideo(filename):
-    dstFileName = VIDEO_FOLD + "\\" + os.path.basename(filename)
-    copyFile(filename, dstFileName)
+    copyFile(srcFileName, dstFileName)
     
 def main(argv):
     global VIDEO_FOLD
-    global UNCLASSIFY_FOLD
-    global CLASSIFY_FOLD
+    global PHOTO_FOLD
     global MOVE_FILE
 
-    fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
+    fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(message)s'
      
-    logging.basicConfig(filename='collect.log',level=logging.INFO, format=fmt)
+    logging.basicConfig(filename='collect.log', level=logging.INFO, format=fmt)
     
     output_dir = "collect"
     config_dir = "PathProcess.txt"
@@ -117,8 +128,7 @@ def main(argv):
 
     if output_dir != "":
         VIDEO_FOLD = output_dir + "\\" + VIDEO_FOLD
-        UNCLASSIFY_FOLD = output_dir + "\\" + UNCLASSIFY_FOLD
-        CLASSIFY_FOLD = output_dir + "\\" + CLASSIFY_FOLD
+        PHOTO_FOLD = output_dir + "\\" + PHOTO_FOLD
 
     try:
         with open(config_dir, "r") as configFile:
@@ -126,8 +136,8 @@ def main(argv):
             while line:
                 mainLoop(str.strip(line))
                 line = configFile.readline()
-    except:
-        print("%s File is not found." % config_dir)
+    except Exception as e:
+        logging.exception(e)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
